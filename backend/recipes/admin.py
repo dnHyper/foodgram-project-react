6735cdde02ae83ae0559c40b38774fc49b17ac6c
Alexey Erёmenko <n8240782@yandex.ru>
2022-06-05 20:42:38 +0300

    Add to recipes/admin field: favorite count

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -4,12 +4,23 @@ from .models import Ingredient, Recipes, Tag
 
 
 class RecipesAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'author', 'display_tag', 'pub_date')
+    list_display = (
+        'pk',
+        'name',
+        'author',
+        'display_tag',
+        'pub_date',
+        'is_favorite'
+    )
     list_editable = ('name', 'author')
     list_display_links = ('pk',)
     search_fields = ('text', 'name')
     list_filter = ('pub_date', 'author', 'tags')
-    fields = ('name', 'text', 'tags', 'author',)
+    fields = ('name', 'text', 'tags', 'author')
+
+    def is_favorite(self, obj):
+        return obj.favorite.count()
+    is_favorite.short_description = 'В избранном'
 
 
 class IngredientAdmin(admin.ModelAdmin):
