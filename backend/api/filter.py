1c50f6b56256264c381b1179031227625b90948b
Alexey Erёmenko <n8240782@yandex.ru>
2022-06-05 20:22:15 +0300

    Add filter to is_favorited and is_in_shopping_cart

--- backend/api/filter.py
+++ backend/api/filter.py
@@ -1,15 +1,33 @@
 from django_filters import CharFilter, FilterSet
+from django_filters import rest_framework as filters
 from recipes.models import Recipes
 
 
 class RecipesFilter(FilterSet):
+    """Фильтрация по автору, тэгу, избранному и добавленному в покупки."""
     author = CharFilter(
         field_name='author__id',
     )
     tags = CharFilter(
         field_name='tags__slug',
     )
+    is_favorited = filters.BooleanFilter(
+        method='favorited_filter'
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='cart_filter'
+    )
+
+    def favorited_filter(self, queryset, name, value):
+        if value:
+            return queryset.filter(favorite__author=self.request.user)
+        return queryset
+
+    def cart_filter(self, queryset, name, value):
+        if value:
+            return queryset.filter(cart__author=self.request.user)
+        return queryset
 
     class Meta:
         model = Recipes
-        fields = ('tags', 'author',)
+        fields = ('tags', 'author', 'is_favorited')
